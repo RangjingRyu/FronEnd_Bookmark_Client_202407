83e48ed948565ff7b4e82ca1c69cd0362936109f
Rangjin <rj.yoo@ilts.com>
2024-07-23 15:30:06 +0900

    layout navbar 통일

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,8 +1,5 @@
-import NavBar from "@/app/components/NavBar";
-
 const page = () => (
   <div>
-     <NavBar />
      <main>
 		    <h1>New Bookmark Form</h1>
 		 </main>   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import NavBar from "./components/NavBar";
-
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <main>
         <h1>Welcome to Bookmarker</h1>
       </main>
